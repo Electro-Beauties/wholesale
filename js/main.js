1e23cf064841fb773db248473f9bcf6e9f196abe
Electro-Beauties <95945798+Electro-Beauties@users.noreply.github.com>
2025-11-30 08:44:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -856,6 +856,9 @@ const images = [
 const btn = document.getElementById("newImageBtn");
 const img = document.getElementById("randomImage");
 
+randomIndex = Math.floor(Math.random() * images.length);
+img.src = images[randomIndex];
+
 btn.addEventListener("click", () => {
     const randomIndex = Math.floor(Math.random() * images.length);
     img.src = images[randomIndex];
